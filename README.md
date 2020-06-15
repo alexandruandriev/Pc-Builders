7f343807f3eab2c3bddd1338a3b5c7be6bb828d8
alexandruandriev <56220209+alexandruandriev@users.noreply.github.com>
2020-06-15 17:12:12 +0200

    Revert "ITS MAM"

--- README.md
+++ README.md
@@ -1,3 +1,2 @@
 # Pc-Builders
 A site that is able to build computers .
-Python 3.8
--- backend/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/home/alex/Projects/Pc-Builders/backend/bin/python
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
